090cfee3539f6092bd899625e43e289b21798142
Philip Guin <philipguin@gmail.com>
2024-08-08 13:13:50 -0500

    Moved HTML markup entirely to decks.js and out of gen_decks_status.py, changing decks.tsv structure. Fixed notes and author being sourced from wrong row on site.

--- docs/decks.js
+++ docs/decks.js
@@ -14,36 +14,104 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
 $(document).ready(function() {
 
-    const NUM_DISPLAY_COLUMNS = 5;
+    const NUM_DISPLAY_COLUMNS = 7;
+
+    function isProgressComplete(x) { // NOTE: logic duplicated in gen_decks_status.py
+        x = x.toLowerCase();
+        return x === 'complete' || x === '100' || x === '100%';
+    }
+    function formatName(name, progress) {
+        if (isProgressComplete(progress)) return name;
+        return `${name} (${progress})`
+    }
+
+    function iconForLinkUrl(url) {
+        if (url.startsWith('https://store.steampowered.com')) return 'icons/steam.svg';
+        else if (url.startsWith('https://www.gog.com')) return 'icons/gog.png';
+        else if (url.startsWith('https://store.epicgames.com')) return 'icons/epic_games.png';
+        else return 'icons/external_link.svg';
+    }
+    function formatLinks(links) {
+        if (!links) return '';
+        let result = `<div class="store-links">`;
+        for (link of links.split('|')) {
+            result += `<a href="${link}" target="_blank" rel="noopener noreferrer">`;
+            result += `<img src="${iconForLinkUrl(link)}" width="16" height="16">`;
+            result += `</a>`;
+        }
+        result += '</div>';
+        return result;
+    }
+
+    function getDifficultyIndex(value) {
+        if (value === null || value === undefined || value == '') return 0;
+
+        let matchUnknown = /^[\?\s]+$/.test(value);
+        if (matchUnknown) {
+            return 0;
+        }
+        let matchSingle = /^\s*(\d+)(\s*\?)?\s*$/.exec(value); // (e.g., "12" or "12?")
+        if (matchSingle) {
+            return parseInt(matchSingle[1], 10);
+        }
+        let matchRange = /^\s*(\d+)\s*\-\s*(\d+)(\s*\?)?\s*$/.exec(value); // (e.g., "12-15" or "12-15?")
+        if (matchRange) {
+            let num1 = parseInt(matchRange[1], 10);
+            let num2 = parseInt(matchRange[2], 10);
+            return Math.round((num1 + num2) / 2);
+        }
+        throw new Error(`Difficulty format not recognized: "${value}"`);
+    }
+    function formatDifficulty(difficulty, diffSrc) {
+
+        const difficultyColors = [
+            '#FFFFFF', '#00FF00', '#32FF00', '#64FF00', '#96FF00', '#C8FF00',
+            '#FFFF00', '#FFC800', '#FF9600', '#FF6400', '#FF3200', '#FF0000',
+        ]
+        let color = difficultyColors[getDifficultyIndex(difficulty)];
+        if (typeof difficulty === 'string') difficulty = difficulty.replace("-", "&#8209;"); // non-breaking hyphen
+
+        difficulty = `<font color="${color}">${difficulty}</font>`;
+        if (diffSrc) {
+            difficulty = `${difficulty}&nbsp;<sub>${diffSrc}</sub>`;
+        }
+        return difficulty;
+    }
+
+    function formatSortedness(str) {
+        if (str.endsWith('?')) str = `<font color="#f8f">${str.substring(0, str.length - 1)}</font>`;
+        return str;
+    }
+
+    function formatQuality(str) {
+        if (str.endsWith('?')) str = `<font color="#f8f">${str.substring(0, str.length - 1)}</font>`;
+        return str;
+    }
+
+    function formatMoreInfo(template, childData) {
+        return template
+            .replace('\{\{deck_author\}\}', childData[0] || 'Unknown')
+            .replace('\{\{notes\}\}', childData[1] || '')
+        ;
+    }
 
     $.ajax({
         url: 'decks.tsv',
         dataType: 'text',
         success: function(data) {
-
-            function iconForLinkUrl(url) {
-                if (url.startsWith('https://store.steampowered.com')) return 'icons/steam.svg';
-                else if (url.startsWith('https://www.gog.com')) return 'icons/gog.png';
-                else if (url.startsWith('https://store.epicgames.com')) return 'icons/epic_games.png';
-                else return 'icons/external_link.svg';
-            }
-            function formatLinks(links) {
-                if (!links) return '';
-                let result = '<div class="store-links">';
-                for (link of links.split(';')) {
-                    result += '<a href="' + link + '" target="_blank" rel="noopener noreferrer">';
-                    result += '<img src="' + iconForLinkUrl(link) + '" width="16" height="16">';
-                    result += '</a>';
-                }
-                result += '</div>';
-                return result;
-            }
-
             let lines = data.split('\n').map(line => line.split('\t'));
             let tableRows = lines.map(line => {
-                return ['', line[0], formatLinks(line[1])].concat(line.slice(2, NUM_DISPLAY_COLUMNS));
+                return [
+                    '',
+                    formatName(line[0], line[1]),
+                    formatLinks(line[2]),
+                    formatDifficulty(line[3], line[4]),
+                    formatSortedness(line[5]),
+                    formatQuality(line[6]),
+                ];
             });
             let tableRowChildren = lines.map(line => {
                 return line.slice(NUM_DISPLAY_COLUMNS);
@@ -76,10 +144,6 @@ $(document).ready(function() {
                 data: tableRows,
     		});
 
-            function format(template, childData) {
-                return template.replace('\{\{notes\}\}', childData[0] || '')
-                               .replace('\{\{deck_author\}\}', childData[1] || 'Unknown');
-            }
             $.get('decks-row-expand.html', function(template) {
 
                 table.on('click', 'td.dt-control', function(e) {
@@ -89,11 +153,10 @@ $(document).ready(function() {
                     if (row.child.isShown()) {
                         row.child.hide();
                     } else {
-                        row.child(format(template, tableRowChildren[tr.index()])).show();
+                        row.child(formatMoreInfo(template, tableRowChildren[row.index()])).show();
                     }
                 });
             });
-
         }
     });
 });
--- scripts/gen_decks_status.py
+++ scripts/gen_decks_status.py
@@ -18,7 +18,7 @@ def scrape_info(yaml_file):
         return yaml.safe_load(file)
 
 
-def is_progress_complete(x):
+def is_progress_complete(x): # NOTE: logic duplicated in decks.js
     x = x.lower()
     return x == 'complete' or x == '100' or x == '100%'
 
@@ -42,86 +42,33 @@ def output_html_metrics(output_file, yaml_infos):
 
 def output_tsv_decks(output_file, yaml_infos):
 
-    difficulty_colors = [
-        '#FFFFFF',
-        '#00FF00',
-        '#32FF00',
-        '#64FF00',
-        '#96FF00',
-        '#C8FF00',
-        '#FFFF00',
-        '#FFC800',
-        '#FF9600',
-        '#FF6400',
-        '#FF3200',
-        '#FF0000',
-    ]
-
-    def get_difficulty_index(value):
-
-        if value is None: return 0
-        if type(value) is int: return value
-        if type(value) is float: return round(value)
-        if type(value) is not str: raise ValueError(f'Difficulty of unhandled type {type(value)}"')
-
-        match_unknown = re.match(r'^[\?\s]+$', value)
-        if match_unknown:
-            return 0
-
-        # Check if the input matches the first format (e.g., "12" or "12?")
-        match_single = re.match(r'^\s*(\d+)(\s*\?)?\s*$', value)
-        if match_single:
-            return int(match_single.group(1))
-        
-        # Check if the input matches the second format (e.g., "12-15" or "12-15?")
-        match_range = re.match(r'^\s*(\d+)\s*\-\s*(\d+)(\s*\?)?\s*$', value)
-        if match_range:
-            num1 = int(match_range.group(1))
-            num2 = int(match_range.group(2))
-            return round((num1 + num2) / 2)
-        
-        raise ValueError(f'Difficulty format not recognized: "{value}"')
-
     is_first = True
     for _, info in yaml_infos:
         name = info.get('name') or ''
-        store_links = info.get('store-links')
-        difficulty = info.get('difficulty')
-        difficulty_source = info.get('difficulty-source')
         progress = info.get('progress') or '??%'
+        store_links = info.get('store-links')
+        difficulty = info.get('difficulty') or ''
+        difficulty_source = info.get('difficulty-source') or ''
         sortedness = info.get('sortedness') or ''
         quality = info.get('quality', '') or ''
-        notes_and_sources = info.get('notes-and-sources') or '' # never allow None
         deck_author = info.get('deck-author') or ''
+        notes_and_sources = info.get('notes-and-sources') or ''
 
-        if type(quality) is str and quality.endswith('?'): quality = f'<font color="#f8f">{quality[:-1]}</font>'
-        if type(sortedness) is str and sortedness.endswith('?'): sortedness = f'<font color="#f8f">{sortedness[:-1]}</font>'
-
-        title = name
-        if not is_progress_complete(progress): title = f'{title} ({progress})'
-
-        links_out = '' if not store_links else ';'.join(store_links)
-
-        if difficulty is None:
-            difficulty = ''
-        else:
-            diff_color = difficulty_colors[get_difficulty_index(difficulty)]
-            if type(difficulty) is str: difficulty = difficulty.replace("-", "&#8209;") # non-breaking hyphen
-            difficulty = f'<font color="{diff_color}">{difficulty}</font>'
-            if difficulty_source:
-                difficulty = f'{difficulty}&nbsp;<sub>{difficulty_source}</sub>'
+        links_out = '' if not store_links else '|'.join(store_links)
 
         if is_first: is_first = False
         else: output_file.write('\n')
 
         row = [
-            title,
+            name,
+            progress,
             links_out,
             difficulty,
+            difficulty_source,
             sortedness,
             quality,
-            notes_and_sources,
             deck_author,
+            notes_and_sources, # most likely to cause format error, so we'll put it last
         ]
         output_file.write('\t'.join(str(x) for x in row))
 
